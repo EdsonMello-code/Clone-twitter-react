b92d3beea409060b0b85083d3f31aa48c61531bc
Edson M. Souza <edsonmelo754@gmail.com>
2020-07-24 13:27:39 -0300

    MenuBar component terminated

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import Main from '../Main'
+import MenuBar from '../MenuBar'
+
 
 import { Container, Wrapper } from './styles';
 
 const Layout: React.FC = () => {
   return <Container>
     <Wrapper>
-      {/* <MenuBar /> */}
+      <MenuBar />
       <Main />
       {/* <SideBar /> */}
     </Wrapper>
