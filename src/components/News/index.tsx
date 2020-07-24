e583154538769c0678e8cb68902348ff60b80e69
Edson M. Souza <edsonmelo754@gmail.com>
2020-07-24 15:56:41 -0300

    Twitter clone completed

---
+++ src/components/News/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const News: React.FC = () => {
+  return (
+    <Container>
+      <span>Assunto do momento no Brasil</span>
+      <strong>Bootcamp da Rockeatseat</strong>
+    </Container>
+  )
+}
+
+export default News;
